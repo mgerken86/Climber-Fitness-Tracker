57666268ddf81764619f46e5f71f4dea1ce43ef7
Mark Gerken <mgerken86@gmail.com>
2022-06-14 09:16:48 -0600

    working on Users auth. Current version has issues with logging in

--- server.js
+++ server.js
@@ -1,29 +1,44 @@
 /////////////////////////////////////////////
 // Import Dependencies
 /////////////////////////////////////////////
-require("dotenv").config(); // Load ENV Variables
-const express = require("express"); // import express
-const morgan = require("morgan"); //import morgan
-const methodOverride = require("method-override");
-const path = require("path");
-const athletesRouter = require("./controllers/athletes");
-const workoutsRouter = require("./controllers/workouts");
-// const usersRouter = require('./controllers/users')
+require("dotenv").config() 
+const express = require("express"); 
+const morgan = require("morgan"); 
+const methodOverride = require("method-override")
+const path = require("path")
+const athletesRouter = require("./controllers/athletes")
+const workoutsRouter = require("./controllers/workouts")
+const usersRouter = require('./controllers/users')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 
 /////////////////////////////////////////////////
 // Create Express Application Object Bind Liquid Templating Engine
 /////////////////////////////////////////////////
 const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
 
-
 // MIDDLEWARE
+app.use(morgan('tiny'))
 app.use(methodOverride("_method"))
-app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public")); // serve files from public statically
+// middleware to use Session
+// middleware to setup session
+app.use(session({
+      secret: process.env.SECRET,
+      store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+      saveUninitialized: true,
+      resave: false,
+    })
+  );
 
+// Use the routers from controllers directory
 app.use('/athletes', athletesRouter)
 app.use('/workouts', workoutsRouter)
-// app.use('/users', usersRouter)
+app.use('/users', usersRouter)
+
+// log-in / sign-up page
+app.use('/', (req, res) => res.render('users/login.liquid'))
 
 
 // Listen Route
